bf2dfb99ff4a024ca4e243f531adbefff5813200
gooinsung <gooinsung96@gmail.com>
2024-01-14 16:54:51 +0900

    first commit

---
+++ src/main/java/com/bali/gitaction_test/GitactionTestApplication.java
@@ -0,0 +1,13 @@
+package com.bali.gitaction_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitactionTestApplication.class, args);
+	}
+
+}
