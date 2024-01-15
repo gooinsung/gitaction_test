2602b77984710ca2f798f06656a4ce130146e333
구인성 <gooinsung>
2024-01-15 17:19:14 +0900

    git action log test

--- src/main/java/com/bali/gitaction_test/TestController.java
+++ src/main/java/com/bali/gitaction_test/TestController.java
@@ -3,6 +3,7 @@ package com.bali.gitaction_test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +13,10 @@ public class TestController {
     public ResponseEntity<String> test(){
         return ResponseEntity.status(HttpStatus.OK).body("Hello~");
     }
+
+    @GetMapping("/hi/{message}")
+    public ResponseEntity<String> test (@PathVariable String message){
+        return ResponseEntity.status(HttpStatus.OK).body(message);
+    }
+
 }
